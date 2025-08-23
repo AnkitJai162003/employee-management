1de3cf7d4ac9d3c81b9acdb89ce07047e46e5691
Ankit <ankitjai16102003@gmail.com>
2025-08-23 22:14:09 +0530

    Service-Layer

--- employee-management/src/main/java/com/example/employee_management/service/EmployeeService.java
+++ employee-management/src/main/java/com/example/employee_management/service/EmployeeService.java
@@ -1,5 +1,34 @@
 package com.example.employee_management.service;
 
+import org.springframework.stereotype.Service;
+import com.example.employee_management.entity.Employee;
+import java.util.*;
+
+import com.example.employee_management.repository.EmployeeRepository;
+
+@Service
 public class EmployeeService {
 
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository){
+        this.employeeRepository=employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees(){
+            return employeeRepository.findAll();
+    }
+
+    public Employee saveEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteById(id);
+    }
+    
 }
