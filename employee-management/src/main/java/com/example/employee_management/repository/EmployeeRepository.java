7070e4dba140144810e3e4b24353defada5d3662
Ankit <ankitjai16102003@gmail.com>
2025-08-23 17:36:00 +0530

    Employee-Repo

--- employee-management/src/main/java/com/example/employee_management/repository/EmployeeRepository.java
+++ employee-management/src/main/java/com/example/employee_management/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.example.employee_management.repository;
 
-public interface EmployeeRepository {
+import com.example.employee_management.entity.Employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
 }
